3dd1b602c643964fa5759ee91a14709411d5b701
Alexander Sundström <alexander.sundstrom@gmail.com>
2018-04-23 09:43:20 +0200

    PDFBox added, returning PDF

--- src/main/java/com/pdf/test/PDFController.java
+++ src/main/java/com/pdf/test/PDFController.java
@@ -1,13 +1,43 @@
 package com.pdf.test;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @RestController
 public class PDFController {
 
-    @RequestMapping("/pdf")
-    public String getHello() {
-        return "Hi";
+    @RequestMapping(value = "/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> getHello() {
+        try (PDDocument document = new PDDocument()) {
+            PDPage page = new PDPage();
+            document.addPage(page);
+            document.save("blank.pdf");
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Disposition", "inline; filename=citiesreport.pdf");
+
+            File file = new File("blank.pdf");
+
+            return ResponseEntity
+                    .ok()
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(new InputStreamResource(new FileInputStream(file)));
+
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+        return ResponseEntity.ok().build();
     }
 }
