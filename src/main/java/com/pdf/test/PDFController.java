63ae979290396e3137bb6095ccbf6c2e0319c7d4
Alexander Sundström <alexander.sundstrom@gmail.com>
2018-04-23 10:04:59 +0200

    PDF Generated with Header

--- src/main/java/com/pdf/test/PDFController.java
+++ src/main/java/com/pdf/test/PDFController.java
@@ -2,6 +2,9 @@ package com.pdf.test;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -9,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -18,15 +20,25 @@ import java.io.IOException;
 public class PDFController {
 
     @RequestMapping(value = "/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<InputStreamResource> getHello() {
+    public ResponseEntity<InputStreamResource> getPDF() {
+        PDPageContentStream contentStream = null;
         try (PDDocument document = new PDDocument()) {
             PDPage page = new PDPage();
             document.addPage(page);
-            document.save("blank.pdf");
 
+            PDFont font = PDType1Font.HELVETICA_BOLD;
+            contentStream = new PDPageContentStream(document, page);
+
+            contentStream.beginText();
+            contentStream.setFont(font, 14);
+            contentStream.newLineAtOffset(100, 700);
+            contentStream.showText("Hello World");
+            contentStream.endText();
+            contentStream.close();
             HttpHeaders headers = new HttpHeaders();
             headers.add("Content-Disposition", "inline; filename=citiesreport.pdf");
 
+            document.save("blank.pdf");
             File file = new File("blank.pdf");
 
             return ResponseEntity
