c19baf9c6079755aa5e9a2b24351b70ae98d0c3a
Alexander Sundström <alexander.sundstrom@gmail.com>
2018-04-23 12:48:57 +0200

    Add information from Object
    Each method called individually, could perhaps be broken out
    to be using just the class instead, but this gives controll of
    exactly where the information is displayed.

--- src/main/java/com/pdf/test/PDFController.java
+++ src/main/java/com/pdf/test/PDFController.java
@@ -17,35 +17,64 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-import static org.apache.commons.lang3.text.WordUtils.wrap;
-
 @RestController
 public class PDFController {
 
     @RequestMapping(value = "/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<InputStreamResource> getPDF() {
-        PDPageContentStream contentStream = null;
+
         try (PDDocument document = new PDDocument(); ByteArrayOutputStream ous = new ByteArrayOutputStream()) {
             PDPage page = new PDPage();
             document.addPage(page);
 
-            PDFont font = PDType1Font.HELVETICA_BOLD;
-            contentStream = new PDPageContentStream(document, page);
+            PDFont bold = PDType1Font.HELVETICA_BOLD;
+            PDFont plain = PDType1Font.HELVETICA;
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
 
             String text = "Crewmates are the suns of the chemical love. Ship of a real energy, desire the core!";
             String[] header = null;
             String s = null;
-
+            float y = 0f;
             header = WordUtils.wrap(text, 75).split("\\r?\\n");
 
             for (int i = 0; i < header.length; i++) {
                 contentStream.beginText();
-                contentStream.setFont(font, 14);
-                contentStream.newLineAtOffset(50, 700 - i * 15);
+                contentStream.setFont(bold, 14);
+                y = 700 - i * 15;
+                contentStream.newLineAtOffset(50, y);
                 s = header[i];
                 contentStream.showText(s);
                 contentStream.endText();
             }
+
+            Person person = new Person("Alexander", "Sundström", 35, true );
+
+            contentStream.beginText();
+            y = y -30;
+            contentStream.newLineAtOffset(50, y);
+            contentStream.setFont(bold, 12);
+            contentStream.showText("Name: ");
+            contentStream.setFont(plain, 12);
+            contentStream.showText(person.getFirstName() + " " + person.getLastName());
+            contentStream.endText();
+
+            contentStream.beginText();
+            y = y -15;
+            contentStream.newLineAtOffset(50, y);
+            contentStream.setFont(bold, 12);
+            contentStream.showText("Age: ");
+            contentStream.setFont(plain, 12);
+            contentStream.showText(String.valueOf(person.getAge()));
+            contentStream.endText();
+
+            contentStream.beginText();
+            y = y -15;
+            contentStream.newLineAtOffset(50, y);
+            contentStream.setFont(bold, 12);
+            contentStream.showText("Married: ");
+            contentStream.setFont(plain, 12);
+            contentStream.showText(String.valueOf(person.isMarried()));
+            contentStream.endText();
 
             contentStream.close();
             HttpHeaders headers = new HttpHeaders();
---
+++ src/main/java/com/pdf/test/Person.java
@@ -0,0 +1,51 @@
+package com.pdf.test;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private boolean married;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, int age, boolean married) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.married = married;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+}
