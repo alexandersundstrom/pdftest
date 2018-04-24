5b0e5cc3223b6eb76d36fa0ee52336d36b5ee36e
Alexander Sundström <alexander.sundstrom@gmail.com>
2018-04-24 16:14:54 +0200

    As previous commit

--- src/main/java/com/pdf/test/controller/PDFController.java
+++ src/main/java/com/pdf/test/controller/PDFController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @RestController
 public class PDFController {
@@ -25,24 +26,31 @@ public class PDFController {
     private FOPService fopService;
 
     @RequestMapping(value = "/pdfbox", produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<InputStreamResource> getPDFBox() {
+    public ResponseEntity<InputStreamResource> getPDFBox() throws IOException {
         Person person = new Person("John", "Doe", 43, true);
+        ByteArrayInputStream inputStream = null;
         try (PDDocument document = pdfService.getPersonalDocument(person);
              ByteArrayOutputStream ous = new ByteArrayOutputStream()) {
             document.save(ous);
+            document.close();
             HttpHeaders headers = new HttpHeaders();
             headers.add("Content-Disposition", "inline; filename=example.pdf");
 
             byte[] bytes = ous.toByteArray();
 
+            inputStream = new ByteArrayInputStream(bytes);
             return ResponseEntity
                     .ok()
                     .headers(headers)
                     .contentType(MediaType.APPLICATION_PDF)
-                    .body(new InputStreamResource(new ByteArrayInputStream(bytes)));
+                    .body(new InputStreamResource(inputStream));
 
         } catch (Exception e) {
             System.out.println("Something went wrong: " + e.getMessage());
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
         }
         return ResponseEntity.ok().build();
     }
